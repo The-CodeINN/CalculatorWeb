2b0cfbff398337f0072bbf6a917d1b4bb838bdc6
Richard Emijere <emijere.richard@gmail.com>
2023-12-15 09:28:46 +0100

    Add project files.

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,33 @@
+﻿using CalculatorWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWeb.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Calc());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calc c, string calculate)
+        {
+            if (calculate == "add")
+            {
+                c.total = c.num1 + c.num2;
+            } else if (calculate == "sub")
+            {
+                c.total = c.num1 - c.num2;
+            } else if(calculate == "multiply")
+            {
+                c.total += c.num1 * c.num2;
+            } else
+            {
+                c.total = c.num1 / c.num2;
+            }
+
+            return View(c);
+        }
+    }
+}
---
+++ Models/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorWeb.Models
+{
+    public class Calc
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+        public double total { get; set; }
+
+    }
+}
